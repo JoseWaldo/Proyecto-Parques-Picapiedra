f79180473029eee17ef5922354238b694ee38ea6
camilo-avendano1 <camilo.avendano1@udea.edu.co>
2022-06-01 17:33:50 -0500

    primer actualizacion de prueba de camilo

---
+++ scripts/celdas.js
@@ -0,0 +1,16 @@
+
+class Cell {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+    draw(){
+        if (mouse.x && mouse.y && collision(this, mouse)){
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+            ctx.strokeStyle = 'black';
+        }
+    }
+}
---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+let canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = '#f7e5e1';
+    ctx.fillRect(0, 0, 700, 700);
+    ctx.strokeStle="black";
+
+
+const cellSize = 100; //tamaño de las casillas
+const gameGrid = []; //casillas del tablero
+
+
+
+//bucle para crearcasillas
+function createGrid(){
+    for (let y = cellSize; y < canvas.height; y += cellSize){
+        for (let x = 0; x < canvas.width; x += cellSize){
+            gameGrid.push(new Cell(x, y));
+        }
+    }
+}
+createGrid();                           //dibujamos las casillas
+function handleGameGrid(){
+    for (let i = 0; i < gameGrid.length; i++){
+        gameGrid[i].draw();
+    }
+}
+
